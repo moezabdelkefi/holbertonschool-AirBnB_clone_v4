2471c7d74ebb940baa9a3a8efc7d24c6f529cded
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-02 15:54:28 +0100

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  const amenitiesDict = {};
+
+  $('input[type="checkbox"]').change(function () { // Listen for changes on each input checkbox tag
+    const amenityId = $(this).attr('data-id'); // Get Amenity ID from the DOM
+    const amenityName = $(this).attr('data-name'); // Get Amenity name from the DOM
+
+    if ($(this).is(':checked')) { // If the checkbox is checked
+      amenitiesDict[amenityId] = amenityName; // Store the Amenity ID in the dictionary
+    } else { // If the checkbox is unchecked
+      delete amenitiesDict[amenityId]; // Remove the Amenity ID from the dictionary
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    $('.amenities h4').html(Object.values(amenitiesDict).join(', '));
+  });
+
+  fetch('http://0.0.0.0:5001/api/v1/status/')
+    .then(response => response.json())
+    .then(({ status }) => document.querySelector('#api_status').classList.toggle('available', status === 'OK'))
+    .catch(error => console.error('Error fetching API status:', error));
+});
